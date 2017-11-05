99d274dee1a2ee123c8cbc6473d596c1d21c39b1
Sunil Kumar <sunil.kumar@nescode.com>
2017-11-05 18:28:32 +0530

    [Cleanup] Templates

--- hellodjango/settings/base.py
+++ hellodjango/settings/base.py
@@ -73,9 +73,7 @@ ROOT_URLCONF = 'hellodjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
